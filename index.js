dc78906c2758c2c93efda26d1e6f9f08972d2fa2
Namada Junior <namada871@gmail.com>
2024-06-27 14:30:32 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // code your solution here
 
-superbowlWin = (record) => {
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+function superbowlWin(record) {
   const result = record.find((record) => record.result === "W");
   return !!result ? result.year : undefined;
-};
+}
